2f5a17dc471468127dd4eab4d98b0441fecb4f45
Rachel <rachel8973@gmail.com>
2022-02-12 11:56:42 +0000

    Update testSlot01.lua

--- testSlot01.lua
+++ testSlot01.lua
@@ -3,5 +3,6 @@
 -------------------------------------------------
 print("What's in slot 16?")
 turtle.select(16)
+print(turtle.getItemDetail())
 print(textutils.serialize(turtle.getItemDetail()))
 
